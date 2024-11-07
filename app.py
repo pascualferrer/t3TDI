524430c65b26bc8e5666c07109e676920b19e926
Pascual Ferrer <pascual.ferrer@uc.cl>
2024-11-07 14:39:37 -0300

    Ajustes Render

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def chat():
         }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
